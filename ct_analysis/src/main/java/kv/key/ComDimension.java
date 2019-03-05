a95b0a24e11010f844de2f403a2fc6668e35de50
xuzhong555 <254018158@qq.com>
2019-03-05 17:00:57 +0800

    增加mapreduce代码，清洗数据

---
+++ ct_analysis/src/main/java/kv/key/ComDimension.java
@@ -0,0 +1,52 @@
+package kv.key;
+
+import kv.base.BaseDimension;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class ComDimension extends BaseDimension{
+    private ContactDimension contactDimension = new ContactDimension();
+    private DateDimension dateDimension = new DateDimension();
+
+    public ComDimension(){
+        super();
+    }
+
+    public ContactDimension getContactDimension() {
+        return contactDimension;
+    }
+
+    public void setContactDimension(ContactDimension contactDimension) {
+        this.contactDimension = contactDimension;
+    }
+
+    public DateDimension getDateDimension() {
+        return dateDimension;
+    }
+
+    public void setDateDimension(DateDimension dateDimension) {
+        this.dateDimension = dateDimension;
+    }
+
+    public int compareTo(BaseDimension o) {
+        ComDimension anotherComDimension = (ComDimension) o;
+
+        int result = this.dateDimension.compareTo(anotherComDimension.dateDimension);
+        if(result != 0) return result;
+
+        result = this.contactDimension.compareTo(anotherComDimension.contactDimension);
+        return result;
+    }
+
+    public void write(DataOutput out) throws IOException {
+        contactDimension.write(out);
+        dateDimension.write(out);
+    }
+
+    public void readFields(DataInput in) throws IOException {
+        contactDimension.readFields(in);
+        dateDimension.readFields(in);
+    }
+}
